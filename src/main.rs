270bcb42997da96fea6bfe135b471fec2a85ce8f
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-07-26 19:13:14 -0400

    Make window stay open on panic

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,37 @@
 sixtyfps::include_modules!();
 
+use std::panic::catch_unwind;
+
 fn main() {
     let window = ModdingToolWindow::new();
 
     window.on_extract(|| {
         println!("Extracting...");
-        gm_data_win::main(gm_data_win::Args {
-            extract_audio: true,
-            extract_sprites: true,
-            extract_fonts: true,
-            extract_textures: true,
-            .. Default::default()
-        }, false);
-        println!("Finished Extracting!");
+        let _ = catch_unwind(|| {
+            gm_data_win::main(gm_data_win::Args {
+                extract_audio: true,
+                extract_sprites: true,
+                extract_fonts: true,
+                extract_textures: true,
+                .. Default::default()
+            }, false);
+
+            println!("Finished Extracting!");
+        });
     });
 
     window.on_inject(|| {
         println!("Injecting...");
-        gm_data_win::main(gm_data_win::Args {
-            mod_audio: true,
-            mod_sprites: true,
-            mod_textures: true,
-            .. Default::default()
-        }, false);
-        println!("Finished Injecting!");
+        let _ = catch_unwind(|| {
+            gm_data_win::main(gm_data_win::Args {
+                mod_audio: true,
+                mod_sprites: true,
+                mod_textures: true,
+                .. Default::default()
+            }, false);
+
+            println!("Finished Injecting!");
+        });
     });
 
     window.run();
