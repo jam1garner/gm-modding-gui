6d20874506fd88cfc7707453d1488e257282d2c4
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-07-30 21:49:52 -0400

    Add toggles for what to extract

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,35 @@ sixtyfps::include_modules!();
 
 use std::panic::catch_unwind;
 
+struct Settings {
+    audio: bool,
+    sprites: bool,
+    fonts: bool,
+    textures: bool,
+}
+
+fn get_settings(window: &ModdingToolWindow) -> Settings {
+    Settings {
+        audio: window.get_audio(),
+        sprites: window.get_sprites(),
+        fonts: window.get_fonts(),
+        textures: window.get_textures(),
+    }
+}
+
 fn main() {
     let window = ModdingToolWindow::new();
 
-    window.on_extract(|| {
+    let win = window.clone_strong();
+    window.on_extract(move || {
         println!("Extracting...");
+        let Settings { audio, sprites, fonts, textures } = get_settings(&win);
         let _ = catch_unwind(|| {
             gm_data_win::main(gm_data_win::Args {
-                extract_audio: true,
-                extract_sprites: true,
-                extract_fonts: true,
-                extract_textures: true,
+                extract_audio: audio,
+                extract_sprites: sprites,
+                extract_fonts: fonts,
+                extract_textures: textures,
                 .. Default::default()
             }, false);
 
@@ -20,21 +38,27 @@ fn main() {
         });
     });
 
-    window.on_inject(|| {
+    let win = window.clone_strong();
+    window.on_inject(move || {
         println!("Injecting...");
+        let Settings { audio, sprites, textures, fonts: _ } = get_settings(&win);
         let _ = catch_unwind(|| {
-            println!("Injecting sprites...");
-            gm_data_win::main(gm_data_win::Args {
-                mod_sprites: true,
-                mod_textures: true,
-                .. Default::default()
-            }, false);
+            if sprites | textures {
+                println!("Injecting sprites...");
+                gm_data_win::main(gm_data_win::Args {
+                    mod_sprites: sprites,
+                    mod_textures: textures,
+                    .. Default::default()
+                }, false);
+            }
 
-            println!("Injecting audio...");
-            gm_data_win::main(gm_data_win::Args {
-                mod_audio: true,
-                .. Default::default()
-            }, false);
+            if audio {
+                println!("Injecting audio...");
+                gm_data_win::main(gm_data_win::Args {
+                    mod_audio: audio,
+                    .. Default::default()
+                }, false);
+            }
 
             println!("Finished Injecting!");
         });
