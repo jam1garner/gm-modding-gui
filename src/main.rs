e0de7c6f64894d68158b63040ad72ec9d64caf05
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-08-06 01:19:01 -0400

    Overhaul game detection code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 sixtyfps::include_modules!();
 
 use std::panic::catch_unwind;
+use std::path::Path;
 use std::fmt;
 
 struct Settings {
@@ -22,7 +23,7 @@ fn get_settings(window: &ModdingToolWindow) -> Settings {
 enum SupportedGame {
     SamuraiGunn2,
     RivalsOfAether,
-    Unknown
+    Unknown(String),
 }
 
 impl SupportedGame {
@@ -30,33 +31,80 @@ impl SupportedGame {
         match self {
             SupportedGame::RivalsOfAether => Some("383980"),
             SupportedGame::SamuraiGunn2 => Some("1397790"),
-            SupportedGame::Unknown => None,
+            _ => None,
         }
     }
 
-    fn is_recognized(&self) -> bool {
-        !matches!(self, SupportedGame::Unknown)
+    fn is_recognized_steam(&self) -> bool {
+        !matches!(self, SupportedGame::Unknown(_))
     }
 }
 
 impl fmt::Display for SupportedGame {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let temp;
         write!(f, "{}", match self {
             SupportedGame::SamuraiGunn2 => "Samurai Gunn 2",
             SupportedGame::RivalsOfAether => "Rivals of Aether",
-            SupportedGame::Unknown => "Unknown"
+            SupportedGame::Unknown(name) => { temp = format!("{} (Game ID Unknown)", name); &temp },
         })
     }
 }
 
-fn detected_game() -> SupportedGame {
-    let current_dir = std::env::current_dir().unwrap();
-    let folder_name = current_dir.file_name().unwrap().to_string_lossy();
-    match &*folder_name {
+enum DetectGameError {
+    DataWinFailRead,
+    DataWinFailParse,
+    NoDataWin,
+}
+
+impl fmt::Display for DetectGameError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "None ({})", match self {
+            DetectGameError::DataWinFailParse => "failed to parse data.win",
+            DetectGameError::DataWinFailRead => "data.win could not be read",
+            DetectGameError::NoDataWin => "no data.win",
+        })
+    }
+}
+
+fn detected_game() -> Result<SupportedGame, DetectGameError> {
+    let game_name = if Path::new("data.win").exists() {
+        if let Ok(data_win) = std::fs::read("data.win") {
+            let data_win_file = gm_data_win::take_data_win_file(&data_win);
+
+            macro_rules! get_section {
+                ($section_kind:ident) => {
+                    data_win_file
+                        .iter()
+                        .filter_map(|section| {
+                            if let gm_data_win::file_structs::Section::$section_kind(section) = section {
+                                Some(section)
+                            } else {
+                                None
+                            }
+                        })
+                        .next()
+                        .ok_or(DetectGameError::DataWinFailParse)
+                }
+            }
+            let gen8 = get_section!(Gen8)?;
+            let strg = get_section!(Strg)?;
+
+            strg.get(gen8.game_name_offset)
+                .ok_or(DetectGameError::DataWinFailParse)
+                .map(|name| name.clone())
+        } else {
+            Err(DetectGameError::DataWinFailRead)
+        }
+    } else {
+        Err(DetectGameError::NoDataWin)
+    }?;
+
+    Ok(match &*game_name {
         "Samurai Gunn 2" => SupportedGame::SamuraiGunn2,
         "Rivals of Aether" => SupportedGame::RivalsOfAether,
-        _ => SupportedGame::Unknown,
-    }
+        _ => SupportedGame::Unknown(game_name),
+    })
 }
 
 fn main() {
@@ -65,9 +113,14 @@ fn main() {
     window.set_title_text(format!("GameMaker 2 Modding Tool {}", env!("CARGO_PKG_VERSION")).into());
 
     let game = detected_game();
-    window.set_game_detected(format!("Game Detected: {}", game).into());
+    window.set_game_detected(
+        format!(
+            "Detected Game: {}",
+            game.as_ref().map(ToString::to_string).unwrap_or_else(ToString::to_string)
+        ).into()
+    );
 
-    window.set_allow_install(game.is_recognized());
+    window.set_allow_install(game.as_ref().map(SupportedGame::is_recognized_steam).unwrap_or(false));
 
     let win = window.clone_strong();
     window.on_extract(move || {
@@ -113,7 +166,7 @@ fn main() {
     });
 
     window.on_uninstall(move || {
-        if let Some(game_id) = game.game_id() {
+        if let Some(game_id) = game.as_ref().ok().map(SupportedGame::game_id).flatten() {
             open::that_in_background(format!("steam://validate/{}", game_id));
         } else {
             println!("The current game is unrecognized and thus cannot uninstall mods.");
